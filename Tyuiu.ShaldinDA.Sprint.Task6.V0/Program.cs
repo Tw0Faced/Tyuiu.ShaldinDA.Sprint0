c4f8c0d77a07258b1d0cf14fc2205704fcdc9837
Данил Шалдин <steampanker45@gmail.com>
2024-09-16 17:35:37 +0500

    Task6

---
+++ Tyuiu.ShaldinDA.Sprint.Task6.V0/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.ShaldinDA.Sprint.Task6.V0.Lib;
+
+namespace Tyuiu.ShaldinDA.Sprint.Task6.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arraynums = new int[] { 1, 2, 3, 4, 5, };
+            
+            Console.WriteLine("Сумма элементов массива = " + DataServise.AdditionArray(arraynums));
+            Console.WriteLine("Разность элементов массива = " + DataServise.SubtractionArray(arraynums));
+            Console.WriteLine("Произведение элементов массива = " + DataServise.MultiplicationArray(arraynums));
+            Console.ReadKey();
+        }
+    }
+}
